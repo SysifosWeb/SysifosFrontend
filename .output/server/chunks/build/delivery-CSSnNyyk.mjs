ca7e00ef934a5594d76c66be2955ec9eeaa69164
osmansmith <osman_ahumada@hotmail.com>
2026-05-13 21:09:43 -0400

    chore: clean up build output artifacts and update dependency configuration

--- .output/server/chunks/build/delivery-CSSnNyyk.mjs
+++
@@ -1,6 +0,0 @@
-import { b as buildAssetsURL } from '../nitro/nitro.mjs';
-
-const delivery = "" + buildAssetsURL("delivery.BbVlRW9e.png");
-
-export { delivery as d };
-//# sourceMappingURL=delivery-CSSnNyyk.mjs.map
